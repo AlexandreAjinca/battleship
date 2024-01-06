b51c1f3021417f00fcc8a85af8a973020db4c2e9
AlexandreAjinca <a.ajinca@laposte.net>
2024-01-06 18:46:02 +0100

    Change indentation

--- src/Settings.js
+++ src/Settings.js
@@ -1,94 +1,96 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-function Settings(props){
-    
-    const [boatList,setBoatList] = useState(props.boats.map(a=>({...a})));
-    const [text,setText] = useState("");
-    
-    useEffect(() => {
-        console.log(" effect triggered");
-    });
+function Settings(props) {
+	const [boatList, setBoatList] = useState(props.boats.map((a) => ({ ...a })));
+	const [text, setText] = useState("");
 
-    function updateValue(target,index){
-        const id = target.target.id;
-        const value = target.target.value
-        const newBoatList = boatList.slice();
-        if(id.includes("Name")){
-            newBoatList[index].name = value;
-        }else if(id.includes("Size")){
-            newBoatList[index].size = Number.parseInt(value);
-        }
-        colorButtonSave();
-        setBoatList(newBoatList);
-        setText(id + " modifié");
-    }
+	useEffect(() => {
+		console.log(" effect triggered");
+	});
 
-    function colorButtonSave(){
-        var buttonSave = document.getElementById("buttonSaveBoats");
-        if(buttonSave.style["background-color"] !== "lightGreen")
-            buttonSave.style["background-color"] = "lightGreen";
-    }
+	function updateValue(target, index) {
+		const id = target.target.id;
+		const value = target.target.value;
+		const newBoatList = boatList.slice();
+		if (id.includes("Name")) {
+			newBoatList[index].name = value;
+		} else if (id.includes("Size")) {
+			newBoatList[index].size = Number.parseInt(value);
+		}
+		colorButtonSave();
+		setBoatList(newBoatList);
+		setText(id + " modifié");
+	}
 
-    function addBoat(){
-        setBoatList([
-            ...boatList,
-            {
-                name:"default",
-                size:1,
-            }
-        ])
-        colorButtonSave();
-        setText("Bateau ajouté");
-    }
+	function colorButtonSave() {
+		var buttonSave = document.getElementById("buttonSaveBoats");
+		if (buttonSave.style["background-color"] !== "lightGreen")
+			buttonSave.style["background-color"] = "lightGreen";
+	}
 
-    function deleteBoat(index){
-        setBoatList(
-            boatList.filter((boat,i) => {
-                return i!==index;
-            })
-        );
-        colorButtonSave();
-        setText("bateau supprimé");
-    }
-    
-    const listBoat = boatList.map((boat,index) =>
-        {
-            const idName = "inputName_"+index;
-            const idSize = "inputSize_"+index;
-            return(
-                <li key={index}>
-                    <label htmlFor={idName}>Nom : </label>
-                    <input 
-                        id={idName} 
-                        name={idName}
-                        onChange={(target)=>updateValue(target,index)} 
-                        type="text" 
-                        value={boat.name}></input>
-                    <br/>
-                    <label htmlFor={idSize}>Taille : </label>
-                    <input 
-                        id={idSize}
-                        nae={idSize}
-                        onChange={(target)=>updateValue(target,index)} 
-                        type="number" 
-                        value={boat.size}></input>
-                    <button onClick={() => deleteBoat(index)}>Delete</button>
-                </li>
-            )
-        }   
-    )
+	function addBoat() {
+		setBoatList([
+			...boatList,
+			{
+				name: "default",
+				size: 1,
+			},
+		]);
+		colorButtonSave();
+		setText("Bateau ajouté");
+	}
 
-    return(
-        <div className='settings'>
-            <h2>Settings</h2>
-            <ul>
-                {listBoat}
-            </ul>
-            <button id="addBoatButton" onClick={() => addBoat()} >Add</button>
-            <button id='buttonSaveBoats' onClick={() => props.onClick(boatList)} >Save</button><br />
-            {text}
-        </div>
-    )
+	function deleteBoat(index) {
+		setBoatList(
+			boatList.filter((boat, i) => {
+				return i !== index;
+			})
+		);
+		colorButtonSave();
+		setText("bateau supprimé");
+	}
+
+	const listBoat = boatList.map((boat, index) => {
+		const idName = "inputName_" + index;
+		const idSize = "inputSize_" + index;
+		return (
+			<li key={index}>
+				<label htmlFor={idName}>Nom : </label>
+				<input
+					id={idName}
+					name={idName}
+					onChange={(target) => updateValue(target, index)}
+					type="text"
+					value={boat.name}
+				></input>
+				<br />
+				<label htmlFor={idSize}>Taille : </label>
+				<input
+					id={idSize}
+					nae={idSize}
+					onChange={(target) => updateValue(target, index)}
+					type="number"
+					value={boat.size}
+				></input>
+				<button onClick={() => deleteBoat(index)}>Delete</button>
+			</li>
+		);
+	});
+
+	return (
+		<div className="settings">
+			<h2>Settings</h2>
+			<ul>{listBoat}</ul>
+			<button id="addBoatButton" onClick={() => addBoat()}>
+				Add
+			</button>
+			<button id="buttonSaveBoats" onClick={() => props.onClick(boatList)}>
+				Save
+			</button>
+			<br />
+			{text}
+		</div>
+	);
 }
 
 export default Settings;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Routes, Route, Link,  BrowserRouter as Router} from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 const routs = (
-  <React.StrictMode>
-    < Router >
-    <h1>BATTLESHIP</h1>
-    <Routes>
-      <Route path="/*" element={<App/>}/>
-    </Routes>
-  </Router>
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<Router>
+			<h1>BATTLESHIP</h1>
+			<Routes>
+				<Route path="/*" element={<App />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
+);
 
-ReactDOM.render(routs,document.getElementById('root'));
+ReactDOM.render(routs, document.getElementById("root"));
